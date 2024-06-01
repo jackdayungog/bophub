93e668320d7b56c69835f1f34a2e16fe4a84179c
jackdayungog <139066407+jackdayungog@users.noreply.github.com>
2024-06-01 10:46:41 -0700

    Update espgui.lua

--- espgui.lua
+++ espgui.lua
@@ -3,7 +3,7 @@ local UserInputService = game:GetService("UserInputService")
 local RunService = game:GetService("RunService")
 local TweenService = game:GetService("TweenService")
 
-local localPlayer = game.Workspace:FindFirstChild(localPlayer.Name)
+local localPlayer = game.Workspace:WaitForChild(localPlayer.Name)
 local maxDistance = 100 -- Default max distance for ESP
 local lerpSpeed = 1 -- Default lerp speed
 
