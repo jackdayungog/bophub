b111c99d2e1df4dcf4a3c3771f63cf3ffb82541d
jackdayungog <139066407+jackdayungog@users.noreply.github.com>
2024-06-10 15:05:45 -0700

    Update espgui.lua

--- espgui.lua
+++ espgui.lua
@@ -19,7 +19,7 @@ local noClipEnabled = false
 
 -- GUI Library
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Script GUI", "Sentinel")
+local Window = Library.CreateLib("Script GUI", "GrapeTheme")
 local Tab = Window:NewTab("Main")
 local Section = Tab:NewSection("Scripts")
 
